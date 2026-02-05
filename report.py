9664adf566a508c8972d0bd0a0d44bd342ed83fb
runo313 <ogheneruno313@gmail.com>
2026-02-05 18:32:34 -0500

    update report.py

--- report.py
+++ report.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 report.py
 
@@ -6,13 +7,15 @@ Supports CSV or JSON output, based on the user's chosen format.
 Used as the last step of the pipeline called by main.py.
 """
 
-def produce_report(format):
+def produce_report(results,format):
     """
+    Input:
+        results: one list per sequence 
     1. If format is "csv":
             write a header row using the keys
             write one row per result
     2. If format is "json":
-            write the list of dicts as JSON
+            write the list as list of dicts
     3. If format is not supported:
             return an error message.
     """
