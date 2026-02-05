375075475a5a100eb026e2956f5be1ba45afdc4e
biomekhi <lucas.mekhi@gmail.com>
2026-02-05 18:17:20 -0500

    Update frameshift.py

---
+++ frameshift.py
@@ -0,0 +1,10 @@
+'''
+
+Using the longest sequence from the ORF function \
+Analyzing codons in 3 frames, each of the following offset by 1 codon \
+Looking from a stop codon in frame 0 \ 
+If frame 1 or 2 continue for 30+ amino acids \
+Return that there is a possible frameshift at that site \ 
+Do this for the whole reading frame \
+
+'''
