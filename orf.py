bbb41cd3829775935625762e90888a613028a4c5
emory-foerster <efoerste@charlotte.edu>
2026-02-05 18:29:23 -0500

    Add detect_ORF function for ORF detection
    Implement a function to detect ORFs in nucleotide sequences.

---
+++ orf.py
@@ -0,0 +1,29 @@
+"""
+def detect_ORF( from output of FASTA_IO):
+     """
+     Purpose:
+         To find all ORFs in a sequence using 3 frames and saving them into dictionaries. 
+     Input:
+         sequence (str): nucleotide sequence list from fastaio.py.
+     Output:
+         A dictionary with the reading frames and orfs cooresponding to them
+         - start: start positon of ORF
+         - end: ending position 
+         - length: length of ORF
+         - frame: reading frame 0,1,2
+     High‑level steps:
+         1. Read in sequence 
+         2. For each reading frames (0,1,2)
+           a. initialize tracking variable to store data
+           b. scan sequence through each reading frame one at a time
+           c. scan sequence 3 nucleotides at a time
+           d. start recording sequence when hit ATG codon 
+               - if start codon comes before stop, create new ORF and record nucleotides in both
+           e.stop recording sequence when hit stop codons
+           
+
+         3. collect all ORF from reading frames
+         4. return list of ORF and the additional information 
+     """
+     pass
+  """
