078c2157abf1f93328c9aa6526de1ab09841dc72
biomekhi <lucas.mekhi@gmail.com>
2026-02-12 18:14:51 -0500

    Update frameshift.py

--- frameshift.py
+++ frameshift.py
@@ -5,16 +5,19 @@ Using the longest sequence from the ORF function \
 Analyzing codons in 3 frames, each of the following offset by 1 codon \
 Looking from a stop codon in frame 0 \ 
 If frames 1 or 2 continue for 30+ amino acids \
-Return that there is a possible frameshift at that site \ 
-Do this for the whole reading frame \
+Return that there is a possible frameshift at that site (Research)\ 
+
+We will research how to output a file to show a visual for each detected frameshift\
 '''
 
 import sys
 
 def Frame_detect(ORF, threshold):
   #goals from above 
+	#Output from file research: What do we want the user to see.
 
 
 if __name__ == '__main__':
 	main()
+
 
