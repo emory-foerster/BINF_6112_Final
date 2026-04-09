eb3e93242e9891f63d1de1269af4c29e5d2984a9
biomekhi <lucas.mekhi@gmail.com>
2026-04-08 22:55:07 -0400

    Made some updates to match other tools.
    added reverse complement to orf and changed one of the counts so it matches ncbi orf finder. Cleaned up frameshift hella should work perfect now. Still need to make one for detecting frameshift across all orfs but need your input. ok bye

--- src/orf.py
+++ src/orf.py
@@ -95,20 +95,27 @@ def detect_all_frames(seq: str, min_length: int = 0) -> list[dict]:
         3. Return the combines list
     """
     ORFs = []
-    for i in range(3):
-        codon_list = parse_codons(seq,i)
-        orf = detect_ORF(codon_list, i)
-        if min_length > 0:
-            orf = [o for o in orf if o['length']>= min_length]
-        ORFs.extend(orf)
+    for strand_seq, strand_label in [(seq, '+'),(reverse_complement(seq), '-')]:
+        for i in range(3):
+            codon_list = parse_codons(strand_seq, i)
+            orf = detect_ORF(codon_list, i)
+            if min_length > 0:
+                orf = [o for o in orf if o['length'] - 3 >= min_length]
+            ORFs.extend(orf)
     return ORFs
 
+def reverse_complement(seq: str) -> str:
+    complement = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+    return ''.join(complement[b] for b in reversed(seq))
+
+
 def main():
-    record = read_fasta("../datasets/Covid_GCF_009858895.2/sample.fasta")
+    record = read_fasta('../datasets/Covid_GCF_009858895.2/sequence.fasta')
     seq = record[0]["Sequence"]
-    all_orfs = detect_all_frames(seq)
+    all_orfs = detect_all_frames(seq, min_length=30)
     meaningful_orfs = detect_all_frames(seq, min_length=150)
     print(all_orfs)
+    print(len(all_orfs))
     
 
 
